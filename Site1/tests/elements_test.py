86cec618f0f31a846834173658926008e6b47eba
tanya_s <tanya_s@bk.ru>
2023-04-15 02:44:53 +0300

    add TestCheckBox, CheckBoxPage, CheckBoxPageLocators, generator.py, data.py

---
+++ Site1/tests/elements_test.py
@@ -0,0 +1,29 @@
+import time
+from Site1.pages.elements_page import *
+
+
+class TestElements:
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_field()
+            time.sleep(5)
+            out_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_field()
+            time.sleep(5)
+            assert full_name == out_name
+            assert email == out_email
+            assert current_address == out_current_address
+            assert permanent_address == out_permanent_address
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random()
+            input_checkbox = check_box_page.get_checked_box()
+            output_checkbox = check_box_page.get_output_result()
+            print(input_checkbox)
+            print(output_checkbox)
+            assert input_checkbox == output_checkbox
