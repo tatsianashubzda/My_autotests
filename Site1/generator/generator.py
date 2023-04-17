54848b3b6e33788cf959664967be2f95da6d1c3b
tanya_s <tanya_s@bk.ru>
2023-04-18 00:40:28 +0300

    feat: add new tests in elements_test.py, change elements_page_locators.py, data.py, conftest.py, elements_page.py, generator.py

--- Site1/generator/generator.py
+++ Site1/generator/generator.py
@@ -9,7 +9,13 @@ Faker.seed()
 
 def get_person():
     yield Person(
-        full_name = faker_ru.first_name()+' '+ faker_ru.last_name(),
+        full_name = faker_ru.first_name() + ' ' + faker_ru.last_name(),
         email = faker_ru.email(),
         current_address = faker_ru.address(),
-        permanent_address = faker_ru.address())
+        permanent_address = faker_ru.address(),
+        first_name = faker_ru.first_name(),
+        last_name=faker_ru.last_name(),
+        age=random.randint(1, 100),
+        department=faker_ru.job(),
+        salary=random.randint(100, 1000000)
+    )
--- Site1/tests/elements_test.py
+++ Site1/tests/elements_test.py
@@ -1,9 +1,13 @@
 import time
+import allure
 from Site1.pages.elements_page import *
 
 
+@allure.suite("Main test")
 class TestElements:
+    @allure.feature("Test Text Box")
     class TestTextBox:
+        @allure.title("Test text box")
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
@@ -16,7 +20,10 @@ class TestElements:
             assert current_address == out_current_address
             assert permanent_address == out_permanent_address
 
+
+    @allure.feature("Test Check Box")
     class TestCheckBox:
+        @allure.title("Test check box")
         def test_check_box(self, driver):
             check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
             check_box_page.open()
@@ -27,3 +34,49 @@ class TestElements:
             print(input_checkbox)
             print(output_checkbox)
             assert input_checkbox == output_checkbox
+
+
+    @allure.feature("Test Radio Button")
+    class TestRadioButton:
+
+        @allure.title("Check radio button")
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, "https://demoqa.com/radio-button")
+            radio_button_page.open()
+            with allure.step("check click on the buttons"):
+                #radio_buttons_list = ["yes", "impressive", "no"]
+                radio_button_page.click_on_the_radiobutton('yes')
+                output_yes = radio_button_page.get_output_result()
+                time.sleep(5)
+                radio_button_page.click_on_the_radiobutton('impressive')
+                output_impressive = radio_button_page.get_output_result()
+                time.sleep(5)
+                radio_button_page.click_on_the_radiobutton('no')
+                output_no = radio_button_page.get_output_result()
+                time.sleep(5)
+            print("!!!!!", output_yes, output_no, output_impressive)
+            assert output_yes == "Yes" , "Yes radiobutton is not clickable"
+            assert output_impressive == "Impressive", "Impressive radiobutton is not clickable"
+            assert output_no == "No", "No radiobutton is not clickable"
+
+
+    @allure.feature("Check webtable")
+    class TestWebTable:
+        @allure.title("add new person table")
+        def test_add_person_in_the_table(self, driver):
+            web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+            web_table_page.open()
+            count = random.randint(1, 5)
+            new_person = web_table_page.add_new_person(count)
+            result = web_table_page.check_new_added_person()
+            assert  new_person in result
+
+        @allure.title("check people in the table")
+        def test_check_people_in_the_table(self, driver):
+            web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_people(key_word)
+            table_result = web_table_page.check_people()
+            time.sleep(10)
+            assert  key_word in table_result
