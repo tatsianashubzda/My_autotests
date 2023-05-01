71bc4f62b7ff8f9a5dc6b71c083584e3afdee2b9
tanya_s <tanya_s@bk.ru>
2023-05-01 22:08:29 +0300

    upd generator.py

--- Site1/generator/generator.py
+++ Site1/generator/generator.py
@@ -17,12 +17,58 @@ def get_person():
         last_name=faker_ru.last_name(),
         age=random.randint(1, 100),
         department=faker_ru.job(),
-        salary=random.randint(100, 1000000)
+        salary=random.randint(100, 1000000),
+        mobile=faker_ru.msisdn()
     )
 
+
 def generated_file():
-    path = rf"D:\Zagruzki\AUTO_DOWNLOAD{random.randint(0, 999)}.txt"
+    path = rf"D:\Program\PyCharm_program\MyAutotest\my_autotests\test{random.randint(0, 999)}.txt"
     with open(path, 'w+') as f:
         f.write(f"""Hello World{random.randint(0, 999)}""")
         f.close()
     return f.name, path
+
+
+def generated_subject():
+    subject = ["Hindi", "English", "Maths", "Physics", "Chemistry", "Biology", "Computer Science",
+               "Commerce", "Accounting", "Economics", "Arts", "Social Studies", "History", "Civics"]
+    random.shuffle(subject)
+    new_list = []
+    for i in range(random.randint(1, 4)):
+        new_list.append(subject[i])
+    return new_list
+
+
+def generated_city():
+    random_list = random.choice(["NCR", "Uttar Pradesh", "Haryana", "Rajasthan"])
+    city = {
+        "NCR": ["Delhi", "Gurgaon", "Noida"],
+        "Uttar Pradesh": ["Agra", "Lucknow", "Merrut"],
+        "Haryana": ["Karnal", "Panipat"],
+        "Rajasthan": ["Jaipur", "Jaiselmer", ]
+    }
+    return random_list, city[random_list]
+
+
+def generator_color():
+    yield Color(
+        color_name=["Red", "Blue", "Yellow", "Purple", "White", "Violet",
+                    "Indigo", "Black", "Magenta", "Aqua", "Green"]
+    )
+
+
+def get_time():
+    hours = random.randint(0, 23)
+    minutes = random.randint(0, 3) * 15
+    time_str = f'{hours:02d}:{minutes:02d}'
+    return time_str
+
+
+def generator_date():
+    yield Date(
+        year=faker_en.year(),
+        month=faker_en.month_name(),
+        day=faker_en.day_of_month(),
+        time=get_time()
+    )
