ee131311561e593a230e0cf9c4f78230caf0ad8c
tanya_s <tanya_s@bk.ru>
2023-05-01 21:15:20 +0300

    upd data.py

--- Site1/data/data.py
+++ Site1/data/data.py
@@ -1,7 +1,7 @@
-from dataclasses import  dataclass
+from dataclasses import dataclass
 
 
-@dataclass()
+@dataclass
 class Person:
     full_name: str = None
     email: str = None
@@ -12,3 +12,17 @@ class Person:
     age: int = None
     salary: int = None
     department: str = None
+    mobile: str = None
+
+
+@dataclass
+class Color:
+    color_name: list = None
+
+
+@dataclass
+class Date:
+    day: str = None
+    month: str = None
+    year: str = None
+    time: str = None
