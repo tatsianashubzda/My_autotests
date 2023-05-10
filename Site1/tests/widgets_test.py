1ef787346c6dcc0e8be7b97bdb409fdbd54becd5
tanya_s <tanya_s@bk.ru>
2023-05-10 10:51:51 +0300

    upd widgets_test.py

--- Site1/tests/widgets_test.py
+++ Site1/tests/widgets_test.py
@@ -1,5 +1,8 @@
+import allure
 import pytest
-from pages.widgets_page import *
+import random
+from pages.widgets_page import AccordianPage, AutoCompletePage, DataPickerPage, SliderPage, TabsPage, ProgressBarPage, \
+    ToolTipsPage, MenuPage, SelectMenuPage
 
 
 @allure.suite("Test Widgets Pages")
@@ -15,6 +18,7 @@ class TestWidgets:
             accordian_page = AccordianPage(driver, "https://demoqa.com/accordian")
             accordian_page.open()
             text, check = accordian_page.check_text_in_cards(item)
+            print(text)
             assert text == check, "Title is incorrect"
 
         @pytest.mark.parametrize('item', title_text)
@@ -59,3 +63,103 @@ class TestWidgets:
             input_color = color_page.fill_single_color_field()
             output_color = color_page.check_single_color_field()
             assert input_color == output_color, "Selected color is wrong"
+
+    @allure.feature("Test Date picker Page")
+    class TestDataPicker:
+        @allure.title("Check select date")
+        def test_check_select_date(self, driver):
+            date_page = DataPickerPage(driver, "https://demoqa.com/date-picker")
+            date_page.open()
+            input_date, date_after = date_page.get_select_date()
+            assert input_date == date_after, "Date has not changed"
+
+        @allure.title("Check date and time page")
+        def test_check_date_and_time(self, driver):
+            date_page = DataPickerPage(driver, "https://demoqa.com/date-picker")
+            date_page.open()
+            date_changes = date_page.get_date_and_time()
+            date_after = date_page.get_date_after_changes()
+            print(date_changes)
+            print(date_after)
+            assert date_after == date_changes, "Date has not changed"
+
+    @allure.feature("Test Slider page")
+    class TestSlider:
+        @allure.title("Check slider")
+        def test_check_slider(self, driver):
+            slider_page = SliderPage(driver, "https://demoqa.com/slider")
+            slider_page.open()
+            value_before, value_after = slider_page.slider()
+            assert value_before != value_after, "Slider works incorrect"
+
+    @allure.feature("Test Progress Bar Page")
+    class TestProgressBar:
+
+        @allure.title("Check progress bar")
+        def test_progress_bar(self, driver):
+            progress_bar = ProgressBarPage(driver, "https://demoqa.com/progress-bar")
+            progress_bar.open()
+            value_before, value_after = progress_bar.progress_bar()
+            assert value_before != value_after, "Progress Bar works incorrect"
+
+    @allure.feature('Test Tabs Page')
+    class TestTabsPage:
+        lst = ["What", "Origin", "Use", "More"]
+
+        @pytest.mark.parametrize('value', lst)
+        @allure.title('Check switched tabs')
+        def test_tabs_page(self, driver, value):
+            tabs_page = TabsPage(driver, "https://demoqa.com/tabs")
+            tabs_page.open()
+            button, content = tabs_page.tabs_page(value)
+            assert value == button and len(content) != 0, f"The tab '{button}' was not pressed or text is missing"
+
+    @allure.feature("Test Tool Tips Page")
+    class TestToolTips:
+        lst = [("button", 'You hovered over the Button'),
+               ("field", 'You hovered over the text field'),
+               ("text", 'You hovered over the Contrary'),
+               ("number", 'You hovered over the 1.10.32')
+               ]
+
+        @pytest.mark.parametrize("item", lst)
+        @allure.title("Check tool tips page")
+        def test_tool_tips(self, driver, item):
+            tool_tips_page = ToolTipsPage(driver, "https://demoqa.com/tool-tips")
+            tool_tips_page.open()
+            value, input_text = item
+            text = tool_tips_page.get_info_after_hover(value)
+            assert text == input_text, "'Hover missing or incorrect content'"
+
+    @allure.feature("Test Menu Page")
+    class TestMenu:
+
+        @allure.title("Check menu items")
+        def test_tool_tips(self, driver):
+            menu_page = MenuPage(driver, "https://demoqa.com/menu")
+            menu_page.open()
+            text = menu_page.check_menu()
+            assert text == ['Main Item 1', 'Main Item 2', 'Sub Item', 'Sub Item', 'SUB SUB LIST »', 'Sub Sub Item 1', 'Sub Sub Item 2', 'Main Item 3'], "Menu titles are incorrect"
+
+    @allure.feature("Test Select Menu")
+    class TestSelectMenu:
+        @allure.title("Check select value")
+        def test_select_menu(self, driver):
+            select_menu_page = SelectMenuPage(driver, "https://demoqa.com/select-menu")
+            select_menu_page.open()
+            input_text, output_text = select_menu_page.check_select_value()
+            assert input_text == output_text, "Wrong data"
+
+        @allure.title("Check select one")
+        def test_select_menu(self, driver):
+            select_menu_page = SelectMenuPage(driver, "https://demoqa.com/select-menu")
+            select_menu_page.open()
+            input_text, output_text = select_menu_page.check_select_one()
+            assert input_text == output_text, "Wrong data"
+
+        @allure.title("Check select old style")
+        def test_select_old_style(self, driver):
+            select_menu_page = SelectMenuPage(driver, "https://demoqa.com/select-menu")
+            select_menu_page.open()
+            input_text, output_text = select_menu_page.check_select_old_style()
+            assert input_text == output_text, "Wrong selected value"
